45a0f58dac5e3a1f3ca5b54d4e02cc5d0a80c949
PapaSnags <58458169+PapaSnags@users.noreply.github.com>
2020-12-24 22:20:47 +1100

    Update

--- source/events/ready.js
+++ source/events/ready.js
@@ -1,5 +1,6 @@
 const chalk = require("chalk")
 const config = require("../cfg/config.json")
+
 module.exports = (client) => {
     console.log(chalk.magenta(`Bot Made by Papa.Snags#1555 \nPrefix is ${config.prefix}`));
-  }
+}
