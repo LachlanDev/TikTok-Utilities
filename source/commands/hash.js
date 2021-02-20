aa23d4443bf78d57a41675a99919ce39037158ac
PapaSnags <58458169+PapaSnags@users.noreply.github.com>
2021-02-20 11:45:48 +1100

    updated number format

--- source/commands/hash.js
+++ source/commands/hash.js
@@ -1,13 +1,6 @@
 const discord = require ("discord.js");
 const TikTokScraper = require('tiktok-scraper');
-
-const numformat = n => {
-    if (n < 1e3) return n;
-    if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + "K";
-    if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + "M";
-    if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + "B";
-    if (n >= 1e12) return +(n / 1e12).toFixed(1) + "T";
-    };
+const number = require('easy-number-formatter')
 
 exports.run = (client, message, args) =>{
     (async () => {
@@ -18,9 +11,9 @@ exports.run = (client, message, args) =>{
             .setColor('#b434eb')
             .setTitle(`Hashtag Info - #${hash.challenge.title}`)
             .addField("Name", `#${hash.challenge.title}`, false)
-            .addField("Video Count:", numformat(`${hash.stats.videoCount}`),true)
-            .addField("View Count:", numformat(`${hash.stats.viewCount}`),true)
-            .setFooter("TikTok Utilities by PapaSnags#1555", "https://cdn.discordapp.com/app-icons/779968259671457792/184465ca744886f62b39f100b52de9a0.png?")
+            .addField("Video Count:", number.formatNumber(`${hash.stats.videoCount}`),true)
+            .addField("View Count:", number.formatNumber(`${hash.stats.viewCount}`),true)
+            .setFooter("TikTok Utilities by PapaSnags#8014", "https://cdn.discordapp.com/app-icons/779968259671457792/184465ca744886f62b39f100b52de9a0.png?")
             message.channel.send({embed: hashtag })
         } 
         catch (error) 
@@ -29,7 +22,7 @@ exports.run = (client, message, args) =>{
             .setColor('#FF0000')
             .setTitle(`Error`)
             .addField("An error has occured", error)
-            .setFooter("TikTok Utilities by PapaSnags#1555", "https://cdn.discordapp.com/app-icons/779968259671457792/184465ca744886f62b39f100b52de9a0.png?")
+            .setFooter("TikTok Utilities by PapaSnags#8014", "https://cdn.discordapp.com/app-icons/779968259671457792/184465ca744886f62b39f100b52de9a0.png?")
             message.channel.send({embed: embederr })
         }
     })();
