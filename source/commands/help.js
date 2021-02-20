be9df9118fb783fd11437efc7a978602d3d23ad0
PapaSnags <58458169+PapaSnags@users.noreply.github.com>
2021-02-20 11:45:36 +1100

    added new commands

--- source/commands/help.js
+++ source/commands/help.js
@@ -7,9 +7,11 @@ exports.run = (client, message, args) =>{
     .setURL("https://github.com/PapaSnags/TikTok-Utilities")
     .addFields(
         {name: "Profile", value: "Lookup a TikTok user's profile displaying their information. \n **Usage:** t!profile TikTok \n"},
+        {name: "Feed", value: "Get the latest post from a TikTok user. \n **Usage:** t!feed TikTok \n"},
         {name: "Hashtag", value: "Lookup a TikTok hashtag displaying information. \n **Usage:** t!hash TikTok \n"},
+        {name: "Trending", value: "Get a currently trending post. \n **Usage:** t!trending \n"},
         {name: "Help", value: "Sends This Help Message \n **Usage:** t!help\n"}
         )
-    .setFooter("TikTok Utilities by PapaSnags#1555", "https://cdn.discordapp.com/app-icons/779968259671457792/184465ca744886f62b39f100b52de9a0.png?")
+        .setFooter("TikTok Utilities by PapaSnags#8014", "https://cdn.discordapp.com/app-icons/779968259671457792/184465ca744886f62b39f100b52de9a0.png?")
     message.channel.send({embed: help })
 };
