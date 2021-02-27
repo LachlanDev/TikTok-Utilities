d776e9b7e45d196b8b35370c76396795ed0bb57f
PapaSnags <58458169+PapaSnags@users.noreply.github.com>
2021-02-27 12:43:42 +1100

    Added ☑️ emoji to profiles

--- source/commands/profile.js
+++ source/commands/profile.js
@@ -7,37 +7,29 @@ exports.run = (client, message, args) =>{
         try 
         {
             const user = await TikTokScraper.getUserProfileInfo(args[0]);
+            const userbe = new discord.MessageEmbed()
+            .setColor('#b434eb')
+            if(user.user.verified == true){
+                userbe.setTitle(`User Info - @${user.user.uniqueId} <:Ver:750173291075076197>`)
+            }
+            else{
+                userbe.setTitle(`User Info - @${user.user.uniqueId}`)
+            }
+            userbe.setURL(`https://www.tiktok.com/@${user.user.uniqueId}`)
+            .setThumbnail(user.user.avatarThumb)
+            .addField("Username", `${user.user.uniqueId}`, true)
+            .addField("Nickname", `${user.user.nickname}`, true)
             if(user.user.signature == ''){
-                const userbe = new discord.MessageEmbed()
-                .setColor('#b434eb')
-                .setTitle(`User Info - @${user.user.uniqueId}`)
-                .setURL(`https://www.tiktok.com/@${user.user.uniqueId}`)
-                .setThumbnail(user.user.avatarThumb)
-                .addField("Username", `${user.user.uniqueId}`, true)
-                .addField("Nickname", `${user.user.nickname}`, true)
-                .addField("Bio", `No bio yet.`)
-                .addField("Followers", number.formatNumber(`${user.stats.followerCount}`),true)
-                .addField("Following", number.formatNumber(`${user.stats.followingCount}`),true)
-                .addField("Hearts", number.formatNumber(`${user.stats.heartCount}`),true)
-                .setFooter("TikTok Utilities by PapaSnags#8014", "https://cdn.discordapp.com/app-icons/779968259671457792/184465ca744886f62b39f100b52de9a0.png?")
-                message.channel.send({embed: userbe })
+                userbe.addField("Bio", `No bio yet.`)
             }
-            else
-            {
-                const userbd = new discord.MessageEmbed()
-                .setColor('#b434eb')
-                .setTitle(`User Info - @${user.user.uniqueId}`)
-                .setURL(`https://www.tiktok.com/@${user.user.uniqueId}`)
-                .setThumbnail(user.user.avatarThumb)
-                .addField("Username", `${user.user.uniqueId}`, true)
-                .addField("Nickname", `${user.user.nickname}`, true)
-                .addField("Bio", `${user.user.signature}`)
-                .addField("Followers", number.formatNumber(`${user.stats.followerCount}`),true)
-                .addField("Following", number.formatNumber(`${user.stats.followingCount}`),true)
-                .addField("Hearts", number.formatNumber(`${user.stats.heartCount}`),true)
-                .setFooter("TikTok Utilities by PapaSnags#8014", "https://cdn.discordapp.com/app-icons/779968259671457792/184465ca744886f62b39f100b52de9a0.png?")
-                message.channel.send({embed: userbd })            
+            else{
+                userbe.addField("Bio", `${user.user.signature}`)
             }
+            userbe.addField("Followers", number.formatNumber(`${user.stats.followerCount}`),true)
+            .addField("Following", number.formatNumber(`${user.stats.followingCount}`),true)
+            .addField("Hearts", number.formatNumber(`${user.stats.heartCount}`),true)
+            .setFooter("TikTok Utilities by PapaSnags#8014", "https://cdn.discordapp.com/app-icons/779968259671457792/184465ca744886f62b39f100b52de9a0.png?")
+            message.channel.send({embed: userbe })
         } 
         catch (error) 
         {
